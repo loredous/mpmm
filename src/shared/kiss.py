a0ef64d275aa016374ddebbabe00c178e31fdb34
Jeremy Banker <loredous@loredous.net>
2023-03-12 16:59:55 +0000

    add KISSClient and KISSTCPClient Add basic testing functions to kiss.py
    Signed-off-by: Jeremy Banker <loredous@loredous.net>

--- src/shared/kiss.py
+++ src/shared/kiss.py
@@ -1,4 +1,7 @@
+from abc import ABC, abstractmethod
+import asyncio
 from enum import Enum
+from logging import getLogger, basicConfig, DEBUG
 from pydantic import BaseModel, validator
 
 from typing import Self
@@ -100,3 +103,114 @@ class KISSFrame(BaseModel):
                     decoded_data.append(byte)
 
         return cls(data=bytes(decoded_data), command=decoded_command, port=decoded_port)
+
+
+class KISSClient(ABC):
+
+    def __init__(self, address: str, logger=None):
+        if not logger:
+            logger = getLogger(f'KISSClient[{address}]')
+        self.logger = logger
+        self.logger.debug(f'Setting up KISS Client for address {address}')
+        self.address = address
+
+    async def recieve_callback(self, frame: KISSFrame):
+        self.logger.info(f'Recieved: {frame.data}')
+
+    async def start_listen(self):
+        self.logger.debug('start_listen requested')
+        await self.setup()
+        self.base_loop = asyncio.get_running_loop()
+        self._stop_requested = False
+        self._running = True
+        self.base_loop.create_task(self.listen_loop())
+
+    async def stop_listen(self, close=True, timeout: int = 60):
+        self._stop_requested = True
+        await self._await_stop(timeout)
+        if close:
+            self.close()
+
+    async def _await_stop(self, timeout: int):
+        for _ in range(timeout):
+            if not self._running:
+                return
+            await asyncio.sleep(1)
+        raise RuntimeError(f'Stop was requested on KISSClient[{self.address}] but failed to stop with {timeout}s timeout.')
+
+    @abstractmethod
+    async def listen_loop(self):
+        pass
+
+    @abstractmethod
+    async def setup(self):
+        pass
+
+    @abstractmethod
+    async def close(self):
+        pass
+
+    @abstractmethod
+    async def send(self, frame: KISSFrame):
+        pass
+
+
+class KISSTCPClient(KISSClient):
+    async def setup(self):
+        address_parts = self.address.split(':')
+        self.logger.debug(f'Opening TCP connection to {self.address}')
+        self.reader, self.writer = await asyncio.open_connection(address_parts[0], address_parts[1])
+
+    async def listen_loop(self):
+        while True:
+            if not self.reader:
+                self.logger.exception('listen_loop called without reader!')
+                raise RuntimeError('Issue in listen loop: reader is None')
+            try:
+                async with asyncio.timeout(10):
+                    data = await self.reader.read(1024)
+                    if data:
+                        frame = KISSFrame.decode(data)
+                        self.base_loop.create_task(self.recieve_callback(frame))
+            except TimeoutError:
+                pass
+            if self._stop_requested:
+                self._running = False
+                return
+            await asyncio.sleep(1)
+
+    async def send(self, frame: KISSFrame):
+        self.logger.debug(f'Send requested for frame [{frame}]')
+        if not self.writer:
+            self.logger.exception('send called without writer!')
+            raise RuntimeError('Issue in send: writer is None')
+        self.writer.write(frame.encode())
+        self.writer.write_eof()
+
+    async def close(self):
+        if self.writer:
+            self.writer.close()
+            await self.writer.wait_closed()
+
+
+if __name__ == "__main__":
+    import signal
+
+    async def delay(coro, seconds):
+        await asyncio.sleep(30)
+        await coro
+
+    def close():
+        print('Got Ctrl-C!')
+        exit()
+
+    basicConfig(level=DEBUG)
+    logger = getLogger('KISSTest')
+    logger.info('Starting KISS Test client')
+    client = KISSTCPClient("192.168.0.13:8001")
+    loop = asyncio.get_event_loop()
+    loop.create_task(client.start_listen())
+    loop.add_signal_handler(signal.SIGINT, close)
+    fr = KISSFrame(data=b'\x82\xa0\x9a\x92`h`\x96\x96`\xb0@@t\x9c`\x82\xaa\xb0@\xe2\xae\x92\x88\x8ad@c\x03\xf0@121504z3934.15N/10455.05W-WX3in1Mini U=12.4V.', command=KISSCommand.DATA_FRAME, port=0)
+    loop.create_task(delay(client.send(fr), 30))
+    loop.run_forever()
